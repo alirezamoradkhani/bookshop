16231ac6e760c8cc2e78b54e96034aac234c9ec4
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-21 12:19:20 +0330

    refactor(book): moving search_book to new architecture

--- app/book/services/search_book.py
+++ app/book/services/search_book.py
@@ -1,13 +1,17 @@
 from fastapi import HTTPException
 import app.book.schemas.inputs as inputs
+from app.book.models.enums import Category
 
 from app.unit_of_work import UnitOfWork
 
-async def search_books(uow: UnitOfWork, search: inputs.BookSearch):
+async def search_books(uow: UnitOfWork
+                ,title: str | None = None
+                ,authors_id: list[int] | None = None
+                ,category: Category | None = None):
     async with uow:
         books = await uow.book.search_books(
-            title=search.title,
-            category=search.category,
-            author_ids=search.authors_id
+            title=title,
+            category=category,
+            author_ids=authors_id
         )
         return books
