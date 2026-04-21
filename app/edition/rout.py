72094dea150edd903a15a19a69d9c5950bbd0c3f
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-21 15:00:39 +0330

    refactor(edition): moving update_price to new architecture

--- app/edition/rout.py
+++ app/edition/rout.py
@@ -4,6 +4,7 @@ from app.get_unit_of_work import get_uow
 from app.edition.schemas.inputs import EditionCreate
 from app.edition.services.create_edition import create_edition
 from app.edition.services.update_amount import update_amount
+from app.edition.services.update_price import update_price
 
 
 router = APIRouter(prefix="/editions", tags=["editions"])
@@ -15,3 +16,7 @@ async def add_edition(edition: EditionCreate,uow = Depends(get_uow),token_data =
 @router.patch("/amount")
 async def change_amount(new_amount: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await update_amount(uow=uow,token_data=token_data,new_amount=new_amount,edition_id=edition_id)
+
+@router.patch("/price")
+async def change_price(new_price: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+    return await update_price(uow=uow,token_data=token_data,new_price=new_price,edition_id=edition_id)
