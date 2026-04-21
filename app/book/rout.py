b38f2e487d4950a0e1f05b42c9db9fdf504dc1f6
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-21 11:52:57 +0330

    refactor(book): moving delete_book to new architecture

--- app/book/rout.py
+++ app/book/rout.py
@@ -4,6 +4,7 @@ from app.get_unit_of_work import get_uow
 from app.book.schemas import inputs, outputs
 from app.book.services.create_book import create_book
 from app.book.services.update_book import update_book
+from app.book.services.delete_book import delete_book
 
 
 router = APIRouter(prefix="/books", tags=["books"])
@@ -15,3 +16,7 @@ async def add_book(new_book:inputs.BookCreate,uow = Depends(get_uow),toke_data =
 @router.patch("update", response_model= outputs.BookResponse)
 async def Update_book(book_id :int,book_update:inputs.BookUpdate,uow = Depends(get_uow),toke_data = Depends(get_current_user)):
     return await update_book(uow=uow,token_data=toke_data,book_id=book_id,book_update=book_update)
+
+@router.delete("/",response_model=outputs.BookResponse)
+async def remove_book(book_id: int,uow = Depends(get_uow),toke_data = Depends(get_current_user)):
+    return await delete_book(uow=uow,token_data=toke_data,book_id=book_id)
