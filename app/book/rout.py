26deadb48dd6cd242b9e7e2b06c4900a2f897b10
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-21 11:48:07 +0330

    refactor(book): moving update_book to new architecture

--- app/book/rout.py
+++ app/book/rout.py
@@ -3,6 +3,7 @@ from app.security import get_current_user
 from app.get_unit_of_work import get_uow
 from app.book.schemas import inputs, outputs
 from app.book.services.create_book import create_book
+from app.book.services.update_book import update_book
 
 
 router = APIRouter(prefix="/books", tags=["books"])
@@ -10,3 +11,7 @@ router = APIRouter(prefix="/books", tags=["books"])
 @router.post("add",response_model=outputs.BookResponse)
 async def add_book(new_book:inputs.BookCreate,uow = Depends(get_uow),toke_data = Depends(get_current_user)):
     return await create_book(uow=uow,new_book=new_book,token_data=toke_data)
+
+@router.patch("update", response_model= outputs.BookResponse)
+async def Update_book(book_id :int,book_update:inputs.BookUpdate,uow = Depends(get_uow),toke_data = Depends(get_current_user)):
+    return await update_book(uow=uow,token_data=toke_data,book_id=book_id,book_update=book_update)
