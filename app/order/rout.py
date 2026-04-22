d89c2a448110aa23742b04f3b59502ecbdacd738
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-22 16:56:58 +0330

    feature(orderedition) adding reject order edition endpoint

--- app/order/rout.py
+++ app/order/rout.py
@@ -5,6 +5,7 @@ from app.order.serivices.user.create_oreder import create_order
 from app.order.serivices.user.cancel_order import cancel_order
 from app.order.serivices.author.get_orderedition import get_order_edition
 from app.order.serivices.author.accept_order_edition import accept_order_edition
+from app.order.serivices.author.reject_order_edition import reject_order_edition
 from app.order.schemas.outputs import OrderResponse
 
 
@@ -25,3 +26,7 @@ async def get_ordereditions(uow = Depends(get_uow),token_data = Depends(get_curr
 @router.patch("/author/accept")
 async def Accept_order_edition(order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await accept_order_edition(uow=uow,token_data=token_data,order_edition_id=order_edition_id)
+
+@router.patch("/author/reject")
+async def reject_orderedition(order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+    return await reject_order_edition(uow=uow,token_data=token_data,order_edition_id=order_edition_id)
