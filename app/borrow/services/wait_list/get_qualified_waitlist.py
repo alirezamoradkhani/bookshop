fb990458050ddd6ffdf37e1f08dd69065ea60862
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-23 12:00:48 +0330

    refactor(waitlist): add get qualified waitlist to services

---
+++ app/borrow/services/wait_list/get_qualified_waitlist.py
@@ -0,0 +1,20 @@
+from fastapi import HTTPException
+from app.user.models.enums import UserPlan
+from app.borrow.models.model import Waitlist
+from app.unit_of_work import UnitOfWork
+from datetime import datetime
+
+
+async def get_qualified_waitlist(uow:UnitOfWork,edition_id):
+    async with uow:
+        qualified_waitlist = await uow.waitlist.get_by_edition_id_and_user_plan(edition_id=edition_id,user_plan=UserPlan.PLATINUM)
+        if qualified_waitlist is not None:
+            return qualified_waitlist
+        else:
+            qualified_waitlist = await uow.waitlist.get_by_edition_id_and_user_plan(edition_id=edition_id,user_plan=UserPlan.GOLD)
+            if qualified_waitlist is not None:
+                return qualified_waitlist
+            else:
+                qualified_waitlist = await uow.waitlist.get_by_edition_id_and_user_plan(edition_id=edition_id,user_plan=UserPlan.SILVER)
+                return qualified_waitlist
+
