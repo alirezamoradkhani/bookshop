7ae927590960b7bb5d8dc2e041507fc4f337cbec
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-23 11:37:39 +0330

    refactor(borrow): add return borrow to services

---
+++ app/borrow/services/return_borrow.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException
+import app.borrow.models.model as model
+import app.book.schemas.inputs as inputs
+import app.borrow.models.enums as enums
+from app.user.models.enums import Role,UserPlan
+from datetime import datetime, timedelta
+
+from app.unit_of_work import UnitOfWork
+
+async def return_borrow(uow:UnitOfWork,token_data:dict,borrow_id:int):
+    async with uow:
+        current_user = await uow.baseusers.get_by_id(user_id= token_data["user_id"])
+        if current_user is None:
+            raise HTTPException(status_code=400, detail="Invalid token user")
+        if current_user.role != Role.USER:
+            raise HTTPException(status_code=400, detail="only User have permission to borrow.")
+        borrow = await uow.borrow.get_by_id(borrow_id=borrow_id)
+        if borrow is None:
+            raise HTTPException(status_code=400, detail="borrow not found")
+        if borrow.user_id != current_user.id:
+            raise HTTPException(status_code=403, detail="this not your borrow")
+        await uow.borrow.update_status(borrow=borrow,new_status=enums.BorrowStatus.RETURNED)
+        now = datetime.utcnow()
+        await uow.borrow.set_Return_time(borrow=borrow,return_time=now)
+        return borrow
