f3774dab401c0ba0c1754573206cce2d2f2f7508
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-27 15:22:01 +0330

    feature(exeption): domain ecp use in user authentic

--- app/user/services/authenticate.py
+++ app/user/services/authenticate.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException
 from app.otp import create_otp, verify_otp
 from app.security import verify_password, create_access_token
 import app.user.schemas.inputs as inputs
+from app.exceptions.user.auth import InvalidOTP,InvalidCredentials
 
 from app.unit_of_work import UnitOfWork
 
@@ -10,13 +11,13 @@ async def athenticate(uow:UnitOfWork,user: inputs.UserLogin):
     async with uow:
         user_in = await uow.baseusers.get_by_username(user.username)
         if user_in is None or not verify_password(user.password, user_in.password):
-            raise HTTPException(status_code=400, detail="Invalid username or password")
+            raise InvalidCredentials
         await create_otp(user_in.email)
         return "OTP sent to email"
     
 async def verify_email(uow:UnitOfWork,otp: str,email:str):
     if not verify_otp(email=email,otp=otp):
-            raise HTTPException(status_code=400, detail="Invalid OTP")
+            raise InvalidOTP
     async with uow:
         user_in = await uow.baseusers.get_by_email(email)
         token_data = {
--- app/user/services/delete_account.py
+++ app/user/services/delete_account.py
@@ -1,10 +1,12 @@
 from app.unit_of_work import UnitOfWork
 from app.user.models.enums import UserPlan
 from fastapi import HTTPException
+from app.exceptions.user.auth import InvalidOTP
+
 
 async def delete_account(uow:UnitOfWork,token_data:dict):
     async with uow:
         current_user = await uow.baseusers.get_by_id(token_data["user_id"])
         if current_user is None:
-            raise HTTPException(status_code=400, detail="Invalid token user")
+            raise InvalidOTP
         return await uow.baseusers.soft_delete(current_user)
