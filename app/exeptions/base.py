b9b58ee7de5d6d5dbd10cad4a5eeffebb6272476
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-27 14:40:13 +0330

    feture(exeption): add status code to main model

--- app/exeptions/base.py
+++ app/exeptions/base.py
@@ -1,5 +1,7 @@
 class DomainException(Exception):
-    def __init__(self, message: str, code: str | None = None):
+    status_code: int = 400  # default
+
+    def __init__(self, message: str, code: str):
         self.message = message
         self.code = code
         super().__init__(message)
--- app/workers/all_runner.py
+++ app/workers/all_runner.py
@@ -14,6 +14,7 @@ from app.workers.runners.order.order_cancel_runner import run_order_cancel_consu
 from app.workers.runners.order.order_item_accept_runner import run_order_item_accept_consumer
 from app.workers.runners.order.order_item_reject_runner import run_order_item_reject_consumer
 from app.workers.runners.user.user_create_runner import run_user_create_consumer
+from app.workers.runners.waitlist.waitlist_create_runner import run_waitlist_create_consumer
 
 
 async def all_runner(broker, uow_factory):
@@ -31,5 +32,6 @@ async def all_runner(broker, uow_factory):
     await run_order_item_accept_consumer(broker=broker,uow_factory=uow_factory)
     await run_order_item_reject_consumer(broker=broker,uow_factory=uow_factory)
     await run_user_create_consumer(broker=broker,uow_factory=uow_factory)
+    await run_waitlist_create_consumer(broker=broker,uow_factory=uow_factory)
 
 #ایونت های پابلیش شده با تایپ مشخص رو میگیره و کارهای مورد نیاز اونو انجام میده
