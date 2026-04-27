bb8851cbd55cedab9151e00615f26edcaba299f6
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-27 13:43:55 +0330

    feature(event driven): add order_create_runner to runners

--- app/workers/all_runner.py
+++ app/workers/all_runner.py
@@ -9,6 +9,7 @@ from app.workers.runners.book.book_delete_runner import run_book_delete_consumer
 from app.workers.runners.edition.edition_create_runner import run_edition_create_consumer
 from app.workers.runners.edition.edition_update_runner import run_edition_update_consumer
 from app.workers.runners.edition.edition_delete_runner import run_edition_delete_consumer
+from app.workers.runners.order.order_create_runner import run_edition_create_consumer
 
 
 async def all_runner(broker, uow_factory):
@@ -21,4 +22,5 @@ async def all_runner(broker, uow_factory):
     await run_edition_create_consumer(broker=broker,uow_factory=uow_factory)
     await run_edition_update_consumer(broker=broker,uow_factory=uow_factory)
     await run_edition_delete_consumer(broker=broker,uow_factory=uow_factory)
+    await run_edition_create_consumer(broker=broker,uow_factory=uow_factory)
 #ایونت های پابلیش شده با تایپ مشخص رو میگیره و کارهای مورد نیاز اونو انجام میده
