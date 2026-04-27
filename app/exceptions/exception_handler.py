8575ff07f1b9fea3ffc204eb6def331bd912cb59
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-27 14:54:37 +0330

    fix(exeption): exception handler now return exp

--- app/exceptions/exception_handler.py
+++ app/exceptions/exception_handler.py
@@ -1,10 +1,12 @@
-# interface/api/exception_handlers.py
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from app.exceptions.base import DomainException
 
-async def domain_exception_handler(request: Request, exc: DomainException):
-    return JSONResponse(
-        status_code=getattr(exc, "status_code", 400),
-        content={"error": exc.code, "message": exc.message}
-    )
+async def domain_exception_handler(request: Request, exc: Exception):  # ← Exception
+    if isinstance(exc, DomainException):
+        return JSONResponse(
+            status_code=getattr(exc, "status_code", 400),
+            content={"error": getattr(exc, "code", "UNKNOWN"), "message": getattr(exc, "message", str(exc))}
+        )
+    # اگر Exception دیگری است، می‌توانیم مجدداً raise کنیم یا default response بدهیم
+    raise exc
