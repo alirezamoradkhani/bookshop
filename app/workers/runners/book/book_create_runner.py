369400cba2e78a19c27e376bfc05b69e28ab7fae
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-27 11:15:17 +0330

    feature(event driven): add cretate_book_runner to runners

---
+++ app/workers/runners/book/book_create_runner.py
@@ -0,0 +1,27 @@
+from app.workers.consumers.book.book_create_consumer import BookCreateConsumer
+import json
+import asyncio
+
+async def run_book_create_consumer(broker,uow_factory):
+
+    consumer = BookCreateConsumer
+    while True:
+        try:
+            pubsub = await broker.subscribe("BookCreated")
+
+            async for message in pubsub.listen():
+
+                if message["type"] != "message":
+                    continue
+
+                event = json.loads(message["data"])
+
+                try:
+                    async with uow_factory() as uow:
+                        await consumer.handle(event, uow)
+                except Exception as e:
+                    print(f"consumer error: {e}")
+
+        except Exception as e:
+            print(f"consumer worker restart: {e}")
+            await asyncio.sleep(2)
