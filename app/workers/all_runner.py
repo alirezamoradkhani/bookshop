f30a38163eeee94165cf742c69ab9551dd08b0ac
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-27 14:13:49 +0330

    feature(event driven): add user_create_runner to runners

--- app/workers/all_runner.py
+++ app/workers/all_runner.py
@@ -13,6 +13,8 @@ from app.workers.runners.order.order_create_runner import run_order_create_consu
 from app.workers.runners.order.order_cancel_runner import run_order_cancel_consumer
 from app.workers.runners.order.order_item_accept_runner import run_order_item_accept_consumer
 from app.workers.runners.order.order_item_reject_runner import run_order_item_reject_consumer
+from app.workers.runners.user.user_create_runner import run_user_create_consumer
+
 
 async def all_runner(broker, uow_factory):
     await run_borrow_return_consumer(uow_factory=uow_factory,broker=broker)
@@ -28,4 +30,6 @@ async def all_runner(broker, uow_factory):
     await run_order_cancel_consumer(broker=broker,uow_factory=uow_factory)
     await run_order_item_accept_consumer(broker=broker,uow_factory=uow_factory)
     await run_order_item_reject_consumer(broker=broker,uow_factory=uow_factory)
+    await run_user_create_consumer(broker=broker,uow_factory=uow_factory)
+
 #ایونت های پابلیش شده با تایپ مشخص رو میگیره و کارهای مورد نیاز اونو انجام میده
