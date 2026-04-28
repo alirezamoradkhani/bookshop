7f6352f34e988c47115f2796704313ad99c1f8e7
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-28 16:02:45 +0330

    fix(analystic): resonse model for best edition in sales

--- app/analytics/schemas/outputs.py
+++ app/analytics/schemas/outputs.py
@@ -8,3 +8,11 @@ class Best_edition_in_borrow(BaseModel):
     edition_id: int
     specefic_edition_title: str |None = None
     total_borrow: int
+
+class Best_edition_in_sell(BaseModel):
+    book_id: int
+    book_title: str
+    book_category: str
+    edition_id: int
+    specefic_edition_title: str |None = None
+    total_sales: int
