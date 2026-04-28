842e766313beed0647ca51565ef680b6c40aa3c2
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-28 11:07:32 +0330

    feature(domain exception):add damain exeption to all order/user service

--- app/order/serivices/user/cancel_order.py
+++ app/order/serivices/user/cancel_order.py
@@ -1,30 +1,31 @@
-from fastapi import HTTPException
 from app.unit_of_work import UnitOfWork
 from app.user.models.enums import Role
 from app.order.models import model, enums
 from datetime import datetime
+from app.exceptions.models.user import InvalidTokenUser,OnlyUserHavePrimition,UserPermissionDenied
+from app.exceptions.models.order import OrderNotCancelable,OrderNotFound
 
 
 async def cancel_order(uow:UnitOfWork,order_id: int, token_data: dict):
     async with uow:
         current_user = await uow.baseusers.get_by_id(user_id= token_data["user_id"])
         if current_user is None:
-            raise HTTPException(status_code=400, detail="Invalid token user")
+            raise InvalidTokenUser
         
         order = await uow.order.get_by_id(order_id)
         if order is None:
-                raise HTTPException(status_code=400, detail= "order not found")
+                raise OrderNotFound
 
         if current_user.role == Role.AUTHOR:
-             raise HTTPException(status_code=400, detail="authors can't cancel user's orders.")
+             raise OnlyUserHavePrimition
         await uow.flush()
         order_editions = await uow.orderedition.get_by_order_id(order.id)
         if current_user.role == Role.USER:
             if order.user_id != current_user.id:
-                raise HTTPException (status_code=400, detail="you are not the owner of this order")
+                raise UserPermissionDenied
             for order_edition in order_editions:
                 if order_edition.state not in [enums.OrderItemState.WAITING,enums.OrderItemState.REJECTED,enums.OrderItemState.CANCELED]:
-                    raise HTTPException (status_code=400, detail="you do not have permission to cancel this order ")
+                    raise OrderNotCancelable
             await uow.order.update_order_atate(order=order,new_state=enums.OrderState.CANCELED)
             for order_edition in order_editions:
                 await uow.orderedition.update_state(orderedition=order_edition,new_state=enums.OrderItemState.CANCELED)
--- app/order/serivices/user/create_oreder.py
+++ app/order/serivices/user/create_oreder.py
@@ -3,28 +3,30 @@ from app.unit_of_work import UnitOfWork
 from app.user.models.enums import Role
 from app.order.models import model
 from datetime import datetime
-from app.exceptions.models.user import InvalidOTP
+from app.exceptions.models.user import InvalidTokenUser,OnlyUserHavePrimition
+from app.exceptions.models.edition import EditionNotFound,EditionOutOfStock
+from app.exceptions.models.transaction import InsufficientFunds
 
 async def create_order(uow:UnitOfWork,edition_ids: list[int], token_data: dict):
     async with uow:
         current_user = await uow.baseusers.get_by_id(user_id= token_data["user_id"])
         if current_user is None:
-            raise InvalidOTP
+            raise InvalidTokenUser
         
         if current_user.role != Role.USER:
-            raise HTTPException(status_code=400, detail="Only users can buy books.")
+            raise OnlyUserHavePrimition
         
         final_price = 0
         for edition_id in edition_ids:
             edition = await uow.edition.get_by_id(edition_id)
             if edition is None:
-                raise HTTPException(status_code=404, detail= f"edition with id= {edition_id} not found.")
+                raise EditionNotFound
             if edition.amount <= 0:
-                raise HTTPException(status_code=400, detail=f"edition with id= {edition_id} is out of stock.")
+                raise EditionOutOfStock
             final_price += edition.price
             edition.amount -= 1
         if current_user.wallet_amount < final_price:
-            raise HTTPException(status_code=400, detail="Insufficient funds in wallet.")
+            raise InsufficientFunds
         await uow.baseusers.decrease_wallet_amount(user=current_user,change=final_price)
         new_order = model.Order(
             user_id = current_user.id
