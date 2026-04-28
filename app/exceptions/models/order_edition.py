aa742a86df32a900ace2217330a23cb6cc131c21
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-28 11:02:04 +0330

    feature(domain exception):add damain exeption to all order/author service

--- app/exceptions/models/order_edition.py
+++ app/exceptions/models/order_edition.py
@@ -16,3 +16,10 @@ class OrderEditionPermissionDenied(DomainException):
             message="This is not your order edition",
             code="ORDER_EDITION_PERMISSION_DENIED"
         )
+
+class InvalidChangeStatus(DomainException):
+    status_code=400
+    def __init__(self):
+        super().__init__(
+            message="Invalid change in status",
+            code="INVALID_CHANGE_IN_STATUS")
