539199c1f8369dc168934fff8684ca164b8e7887
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-29 10:56:53 +0330

    fix(cuery):book can have multiple category

--- app/query/fun_record/category/best_category_in_sell.py
+++ app/query/fun_record/category/best_category_in_sell.py
@@ -5,14 +5,13 @@ from app.models import *
 
 async def best_category_in_sell(db: AsyncSession):
     result = await db.execute(
-        select(Book.category.label("categorys"),
-               func.count(Book.category).label("total_sales"))
+        select(BookCategory.category.label("categorys"),
+               func.count(OrderEdition.order_edition_id).label("total_sales"))
+               .join(Book,Book.id == BookCategory.book_id)
                .join(Edition,Edition.book_id == Book.id)
                .join(OrderEdition,OrderEdition.edition_id == Edition.id)
-               .join(Order,Order.id == OrderEdition.order_id)
-               .where(OrderEdition.state == OrderItemState.DONE,
-                      Order.state == OrderState.DONE)
-                      .group_by(Book.category)
+               .where(OrderEdition.state == OrderItemState.DONE)
+                      .group_by(BookCategory.category)
                       .order_by(desc("total_sales")).limit(20)
     )
     return result.all()
