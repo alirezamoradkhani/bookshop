4248bf961aa619183d18d3b47681f932a4c04864
Alireza moradkhani <moradkhani1386@gmail.com>
2026-04-30 16:36:59 +0330

    feature(user):add checks for use in worker

--- app/user/repo/user.py
+++ app/user/repo/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.user.models import model, enums
-from datetime import datetime
+from datetime import datetime,timedelta
 
 class UserRepository:
     def __init__(self, db: AsyncSession):
@@ -19,8 +19,24 @@ class UserRepository:
         result = await self.db.execute(select(model.User).where(model.User.id == id))
         user = result.scalar_one_or_none()
         user.plan = new_plan
+        user.plan_expire = ex
+        return user
+    
+    async def change_user_plan(self,user:model.User,new_plan:enums.UserPlan):
+        user.plan = new_plan
         return user
     
     async def get_plan_by_id(self,user_id:int):
         result = await self.db.execute(select(model.User.plan).where(model.User.id == user_id))
         return result.scalar_one_or_none()
+    
+    async def get_plan_by_exp_date(self,now:datetime):
+        start = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        end = start + timedelta(days=1)
+        result = await self.db.execute(
+            select(model.User)
+            .where(model.User.plan_expire >= start
+                   ,model.User.plan_expire < end
+                   ,model.User.plan != enums.UserPlan.BRONZE)
+                   )
+        return result
---
+++ app/user/services/checks/dowgrade_expired_plan.py
@@ -0,0 +1,7 @@
+from app.unit_of_work import UnitOfWork
+from app.user.models.enums import UserPlan
+from app.user.services.checks.get_expired_user_plan import get_expired_user_plan
+async def downgrdae_expired_plan(uow:UnitOfWork):
+    exp_user_plan = get_expired_user_plan(uow=uow)
+    for user in exp_user_plan:
+        await uow.user.change_user_plan(user=user,new_plan=UserPlan.BRONZE)
