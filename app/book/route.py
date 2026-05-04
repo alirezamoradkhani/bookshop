987bf7180dc71685f9e6f8f54f1ebe17b56366ab
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-04 10:42:55 +0330

    featurre(book): add external serach endpoint

--- app/book/route.py
+++ app/book/route.py
@@ -8,7 +8,11 @@ from app.book.services.command.update_book import update_book
 from app.book.services.command.delete_book import delete_book
 from app.book.services.querys.search_book import search_books
 from app.book.services.querys.get_book_details import book_detail
-
+from app.book.services.external.querys.external_search_book_by_title import external_search_book_by_title
+from app.book.services.external.querys.external_search_book_by_author import external_search_book_by_author
+from app.book.services.external.querys.external_search_book_by_ISBN import external_search_book_by_ISBN
+from app.book.services.external.querys.external_book_detail_by_id import external_book_detail_by_id
+from app.external_API.providers.open_library.get_extternal_services import get_openlibrary_provider
 
 router = APIRouter(prefix="/books", tags=["books"])
 
@@ -30,6 +34,22 @@ async def Search_book(category: str | None = None
                 ,title: str | None = None,uow = Depends(get_uow)):
     return await search_books(uow=uow,category=category,title=title,author_id=author_id)
 
-@router.get("details",response_model=outputs.BookDetails)
+@router.get("/details",response_model=outputs.BookDetails)
 async def get_detail(book_id:int,uow = Depends(get_uow)):
     return await book_detail(uow=uow,book_id=book_id)
+
+@router.get("external/search/by_title")
+async def External_search_book_by_title(title:str, provider = Depends(get_openlibrary_provider)):
+    return await external_search_book_by_title(title=title, provider=provider)
+
+@router.get("external/search/by_author")
+async def External_search_book_by_author(author:str, provider = Depends(get_openlibrary_provider)):
+    return await external_search_book_by_author(author=author, provider=provider)
+
+@router.get("external/search/by_isbn")
+async def External_search_book_by_isbn(isbn:str, provider = Depends(get_openlibrary_provider)):
+    return await external_search_book_by_ISBN(isbn=isbn, provider=provider)
+
+@router.get("external/detail_by_id")
+async def External_book_detail_by_id(book_id:str, provider = Depends(get_openlibrary_provider)):
+    return await external_book_detail_by_id(provider=provider,work_id=book_id)
