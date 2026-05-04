b62182b8230c4ef910b68aff42148f9f7631794e
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-04 12:10:15 +0330

    rafactor(user): login

--- app/user/route.py
+++ app/user/route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.security import get_current_user
-from app.user.services.command.authenticate import athenticate, verify_email
+from app.user.services.command.authenticate import login_by_user_pass, login_by_otp
 from app.user.services.command.create_baseuser import create_user, email_register
 from app.user.services.command.delete_account import delete_account
 from app.user.services.command.upgrade_user_plan import upgrade_plan
@@ -20,13 +20,13 @@ async def Verify_email(email: str,uow = Depends(get_uow)):
 async def Create_user(user: inputs.UserCreate, otp:str, uow = Depends(get_uow)):
     return await create_user(uow=uow,user=user,otp=otp)
 
-@router.post("/register")
+@router.post("/login")
 async def login_step_one(user: inputs.UserLogin,uow = Depends(get_uow)):
-    return await athenticate(uow=uow,user=user)
+    return await login_by_user_pass(uow=uow,user=user)
 
-@router.post("/login", response_model=outputs.TokenResponse)
-async def login(email:str,otp :str, uow = Depends(get_uow)):
-    return await verify_email(uow=uow,otp=otp,email=email)
+# @router.post("/login_by_otp", response_model=outputs.TokenResponse)
+# async def login(email:str,otp :str, uow = Depends(get_uow)):
+#     return await login_by_otp(uow=uow,otp=otp,email=email)
 
 @router.delete("/delete", response_model= outputs.BaseUserResponse)
 async def Delete_account(token_data: dict = Depends(get_current_user), uow = Depends(get_uow)):
--- app/user/services/command/authenticate.py
+++ app/user/services/command/authenticate.py
@@ -6,21 +6,25 @@ from app.exceptions.models.user import InvalidCredentials,InvalidOTP
 from app.unit_of_work import UnitOfWork
 
 
-async def athenticate(uow:UnitOfWork,user: inputs.UserLogin):
+async def login_by_user_pass(uow:UnitOfWork,user: inputs.UserLogin):
     async with uow:
         user_in = await uow.baseusers.get_by_username(user.username)
         if user_in is None or not verify_password(user.password, user_in.password):
             raise InvalidCredentials
-        await create_otp(user_in.email)
-        return "OTP sent to email"
-    
-async def verify_email(uow:UnitOfWork,otp: str,email:str):
-    if not await verify_otp(email=email,otp=otp):
-            raise InvalidOTP
-    async with uow:
-        user_in = await uow.baseusers.get_by_email(email)
+        user_in = await uow.baseusers.get_by_username(name=user.username)
         token_data = {
         "user_id": user_in.id,
     }
-    access_token = create_access_token(token_data)
-    return {"access_token": access_token, "token_type": "bearer"}
+        access_token = create_access_token(token_data)
+        return {"access_token": access_token, "token_type": "bearer"}
+    
+# async def login_by_otp(uow:UnitOfWork,otp: str,email:str):
+#     if not await verify_otp(email=email,otp=otp):
+#             raise InvalidOTP
+#     async with uow:
+#         user_in = await uow.baseusers.get_by_email(email)
+#         token_data = {
+#         "user_id": user_in.id,
+#     }
+#     access_token = create_access_token(token_data)
+#     return {"access_token": access_token, "token_type": "bearer"}
