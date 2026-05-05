ce085e89ee7efdaa648b3fe12c73f1a8590b8a45
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-05 14:18:53 +0330

    feature(edition): add endpoint for import edition from external srevice

--- app/edition/route.py
+++ app/edition/route.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends
 from app.security import get_current_user
 from app.get_unit_of_work import get_uow
+from app.external_API.providers.open_library.get_extternal_services import get_openlibrary_provider
 from app.edition.schemas.inputs import EditionCreate
 from app.edition.services.command.create_edition import create_edition
 from app.edition.services.command.update_amount import update_amount
 from app.edition.services.command.update_price import update_price
 from app.edition.services.command.delete_edition import remove_edition
+from app.edition.services.external.command.import_edition import import_edition
 
 
 router = APIRouter(prefix="/editions", tags=["editions"])
@@ -25,3 +27,7 @@ async def change_price(new_price: int,edition_id : int,uow = Depends(get_uow),to
 @router.delete("/delete")
 async def delete_edition(edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await remove_edition(uow=uow,token_data=token_data,edition_id=edition_id)
+
+@router.post("/external")
+async def Import_edition(book_id: int,external_edition_title:str,uow = Depends(get_uow),token_data = Depends(get_current_user),provider = Depends(get_openlibrary_provider)):
+    return await import_edition(uow=uow,token_data=token_data,book_id=book_id,external_edition_title=external_edition_title,provider=provider)
