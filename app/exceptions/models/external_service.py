278b1b488513ac155ebc7e3426dcf764c88ed47e
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-05 14:14:51 +0330

    feature(exception): external_service

--- app/exceptions/models/external_service.py
+++ app/exceptions/models/external_service.py
@@ -29,3 +29,33 @@ class ExternalServiceUnavailable(DomainException):
             message="External service unavailable",
             code="EXTERNAL_SERVICE_UNAVAILABLE"
         )
+
+
+class ExternalServiceCanNotFound(DomainException):
+    status_code = 404
+
+    def __init__(self):
+        super().__init__(
+            message="External service can not found",
+            code="EXTERNAL_SERVICE_CAN_NOT_FOUND"
+        )
+
+
+class LanguageNotSuported(DomainException):
+    status_code = 400
+
+    def __init__(self):
+        super().__init__(
+            message="Language Not Suported",
+            code="LANGUAGE_NOT_SUPORTED"
+        )
+
+
+class BookAleadyImported(DomainException):
+    status_code = 409
+
+    def __init__(self):
+        super().__init__(
+            message="Boook Already Imported",
+            code="BOOK_ALREADY_IMPORTED"
+        )
