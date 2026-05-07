0033b1cacfa42dee861554c2383be97a7f71be9d
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-07 14:33:47 +0330

    feature(edition): add limiter

--- app/edition/route.py
+++ app/edition/route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from app.security import get_current_user
 from app.get_unit_of_work import get_uow
 from app.external_API.providers.open_library.get_extternal_services import get_openlibrary_provider
@@ -8,26 +8,31 @@ from app.edition.services.command.update_amount import update_amount
 from app.edition.services.command.update_price import update_price
 from app.edition.services.command.delete_edition import remove_edition
 from app.edition.services.external.command.import_edition import import_edition
-
+from app.ratelimiter.limiter import limiter
 
 router = APIRouter(prefix="/editions", tags=["editions"])
 
 @router.post("/")
-async def add_edition(edition: EditionCreate,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def add_edition(request: Request, edition: EditionCreate,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await create_edition(uow=uow,token_data=token_data,edition=edition)
 
 @router.patch("/amount")
-async def change_amount(new_amount: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def change_amount(request: Request, new_amount: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await update_amount(uow=uow,token_data=token_data,new_amount=new_amount,edition_id=edition_id)
 
 @router.patch("/price")
-async def change_price(new_price: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def change_price(request: Request, new_price: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await update_price(uow=uow,token_data=token_data,new_price=new_price,edition_id=edition_id)
 
 @router.delete("/delete")
-async def delete_edition(edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def delete_edition(request: Request, edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await remove_edition(uow=uow,token_data=token_data,edition_id=edition_id)
 
 @router.post("/external")
-async def Import_edition(book_id: int,external_edition_title:str,uow = Depends(get_uow),token_data = Depends(get_current_user),provider = Depends(get_openlibrary_provider)):
+@limiter.limit("5/minute")
+async def Import_edition(request: Request, book_id: int,external_edition_title:str,uow = Depends(get_uow),token_data = Depends(get_current_user),provider = Depends(get_openlibrary_provider)):
     return await import_edition(uow=uow,token_data=token_data,book_id=book_id,external_edition_title=external_edition_title,provider=provider)
