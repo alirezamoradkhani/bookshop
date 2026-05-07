ae2da33a23cbefe659a1eb88242bb4fa8b26f6f2
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-07 14:35:27 +0330

    feature(order): add limiter

--- app/order/route.py
+++ app/order/route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from app.security import get_current_user
 from app.get_unit_of_work import get_uow
 from app.order.serivices.user.command.create_oreder import create_order
@@ -11,37 +11,44 @@ from app.order.serivices.user.querys.get_orders import get_user_orders
 from app.order.schemas.outputs import OrderResponse
 from app.Idempotency.dependency import get_idempotency_handler
 from app.Idempotency.get_idempotency_key import get_idempotency_key
-
+from app.ratelimiter.limiter import limiter
 
 router = APIRouter(prefix="/orders", tags=["orders"])
 
 @router.post("/user/buy",response_model= OrderResponse)
-async def buy(edition_ids: list[int] ,handeler = Depends(get_idempotency_handler), uow = Depends(get_uow),token_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
+@limiter.limit("5/minute")
+async def buy(request: Request, edition_ids: list[int] ,handeler = Depends(get_idempotency_handler), uow = Depends(get_uow),token_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
     # return await create_order(uow=uow,token_data=token_data,edition_ids=edition_ids)
     return await handeler(key=idempotency_key,usecase=create_order,uow=uow,token_data=token_data,edition_ids=edition_ids)
 
 
 
 @router.patch("/user/cancel",response_model= OrderResponse)
-async def Cancel_order(order_id:int ,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def Cancel_order(request: Request, order_id:int ,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await cancel_order(uow=uow,token_data=token_data,order_id=order_id)
 
 @router.get("/user/")
-async def Get_order(uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def Get_order(request: Request, uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await get_user_orders(uow=uow,token_data=token_data)
 
 @router.get("/author/")
-async def get_ordereditions(uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def get_ordereditions(request: Request, uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await get_order_edition(uow=uow,token_data=token_data)
 
 @router.patch("/author/accept")
-async def Accept_order_edition(order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def Accept_order_edition(request: Request, order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await accept_order_edition(uow=uow,token_data=token_data,order_edition_id=order_edition_id)
 
 @router.patch("/author/reject")
-async def reject_orderedition(order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def reject_orderedition(request: Request, order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await reject_order_edition(uow=uow,token_data=token_data,order_edition_id=order_edition_id)
 
 @router.patch("/admin")
-async def confirm_delivery(order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@limiter.limit("5/minute")
+async def confirm_delivery(request: Request, order_edition_id: int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
     return await confirm_delivery_to_courier(uow=uow,token_data=token_data,order_edition_id=order_edition_id)
