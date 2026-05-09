f1ace43680679856035c0f2507bc1b7c24fdb7ce
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-09 16:11:33 +0330

    refactor(mark orderedtion as rejected):removing n+1 query

--- app/order/serivices/checks/mark_orderedition_as_rejected.py
+++ app/order/serivices/checks/mark_orderedition_as_rejected.py
@@ -7,5 +7,7 @@ async def mark_orderedition_as_rejected(uow: UnitOfWork):
     date = now - timedelta(days=2)
     async with uow:
         order_editions = await uow.orderedition.get_by_last_modify_and_state(state=enums.OrderItemState.WAITING,date=date)
-        for order_editioin in order_editions:
-            await uow.orderedition.update_state(orderedition=order_editioin,new_state=enums.OrderItemState.REJECTED)
+        await uow.orderedition.many_update_state(
+            order_edition_ids=[oe.order_edition_id for oe in order_editions],
+            new_state=enums.OrderItemState.REJECTED
+        )
