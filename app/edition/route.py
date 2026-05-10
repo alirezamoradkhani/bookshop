f9c3680ce5a3ee5685055be51fec16870d0a57a6
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-10 16:54:58 +0330

    refactore(edition): add dependency injection for uow

--- app/edition/route.py
+++ app/edition/route.py
@@ -9,30 +9,37 @@ from app.edition.services.command.update_price import update_price
 from app.edition.services.command.delete_edition import remove_edition
 from app.edition.services.external.command.import_edition import import_edition
 from app.ratelimiter.limiter import limiter
+from app.dependency_injection.container import Container
+from dependency_injector.wiring import inject, Provide
 
 router = APIRouter(prefix="/editions", tags=["editions"])
 
 @router.post("/")
 @limiter.limit("5/minute")
-async def add_edition(request: Request, edition: EditionCreate,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@inject
+async def add_edition(request: Request, edition: EditionCreate,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user)):
     return await create_edition(uow=uow,token_data=token_data,edition=edition)
 
 @router.patch("/amount")
 @limiter.limit("5/minute")
-async def change_amount(request: Request, new_amount: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@inject
+async def change_amount(request: Request, new_amount: int,edition_id : int,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user)):
     return await update_amount(uow=uow,token_data=token_data,new_amount=new_amount,edition_id=edition_id)
 
 @router.patch("/price")
 @limiter.limit("5/minute")
-async def change_price(request: Request, new_price: int,edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@inject
+async def change_price(request: Request, new_price: int,edition_id : int,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user)):
     return await update_price(uow=uow,token_data=token_data,new_price=new_price,edition_id=edition_id)
 
 @router.delete("/delete")
 @limiter.limit("5/minute")
-async def delete_edition(request: Request, edition_id : int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@inject
+async def delete_edition(request: Request, edition_id : int,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user)):
     return await remove_edition(uow=uow,token_data=token_data,edition_id=edition_id)
 
 @router.post("/external")
 @limiter.limit("5/minute")
-async def Import_edition(request: Request, book_id: int,external_edition_title:str,uow = Depends(get_uow),token_data = Depends(get_current_user),provider = Depends(get_openlibrary_provider)):
+@inject
+async def Import_edition(request: Request, book_id: int,external_edition_title:str,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user),provider = Depends(Provide[Container.openlibrary])):
     return await import_edition(uow=uow,token_data=token_data,book_id=book_id,external_edition_title=external_edition_title,provider=provider)
