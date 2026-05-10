919ad1347d57becc1394f77062d88523e7e19291
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-10 16:55:19 +0330

    refactore(transaction): add dependency injection for uow

--- app/transaction/route.py
+++ app/transaction/route.py
@@ -9,6 +9,8 @@ from app.transaction.schemas.outputs import BaseUserResponse
 from app.Idempotency.dependency import get_idempotency_handler
 from app.Idempotency.get_idempotency_key import get_idempotency_key
 from app.ratelimiter.limiter import limiter
+from dependency_injector.wiring import inject, Provide
+from app.dependency_injection.container import Container
 
 
 router = APIRouter(prefix="/transaction", tags=["transaction"])
@@ -16,23 +18,27 @@ router = APIRouter(prefix="/transaction", tags=["transaction"])
 
 @router.post("/deposit")
 @limiter.limit("5/minute")
-async def Deposit(request: Request, amount:int, handeler = Depends(get_idempotency_handler), uow = Depends(get_uow),toke_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
+@inject
+async def Deposit(request: Request, amount:int, handeler = Depends(get_idempotency_handler), uow = Depends(Provide[Container.uow]),toke_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
     # return await deposit(uow=uow,token_data=toke_data,amount=amount)
     return await handeler(key=idempotency_key,usecase=deposit,uow=uow,token_data=toke_data,amount=amount)
 
 @router.post("/transfer",response_model=BaseUserResponse)
 @limiter.limit("5/minute")
-async def Transfer(request: Request, amount:int,reciver_id:int ,handeler = Depends(get_idempotency_handler), uow = Depends(get_uow),toke_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
+@inject
+async def Transfer(request: Request, amount:int,reciver_id:int ,handeler = Depends(get_idempotency_handler), uow = Depends(Provide[Container.uow]),toke_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
     # return await transfer(uow=uow,token_data=toke_data,amount=amount,reciver_id=reciver_id)
     return await handeler(key=idempotency_key,usecase=transfer,uow=uow,token_data=toke_data,amount=amount,reciver_id=reciver_id)
 
 @router.get("/info")
 @limiter.limit("5/minute")
-async def wallet_info(request: Request, uow = Depends(get_uow),toke_data = Depends(get_current_user)):
+@inject
+async def wallet_info(request: Request, uow = Depends(Provide[Container.uow]),toke_data = Depends(get_current_user)):
     return await walletinfo(uow=uow,token_data=toke_data)
 
 @router.post("/withdraw",response_model=BaseUserResponse)
 @limiter.limit("5/minute")
-async def Withdraw(request: Request, amount:int, handeler = Depends(get_idempotency_handler), uow = Depends(get_uow),toke_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
+@inject
+async def Withdraw(request: Request, amount:int, handeler = Depends(get_idempotency_handler), uow = Depends(Provide[Container.uow]),toke_data = Depends(get_current_user),idempotency_key: str = Depends(get_idempotency_key)):
     # return await withdraw(uow=uow,token_data=toke_data,amount=amount)
     return await handeler(key=idempotency_key,usecase=withdraw,uow=uow,token_data=toke_data,amount=amount)
