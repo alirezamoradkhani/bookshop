7c61233d3857dc5ae0138cd5fb7740ef03fae54d
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-10 16:54:32 +0330

    refactore(borrow): add dependency injection for uow

--- app/borrow/route.py
+++ app/borrow/route.py
@@ -6,21 +6,26 @@ from app.borrow.services.borrow.borrow_edition import borrow_edition
 from app.borrow.services.borrow.return_borrow import return_borrow
 from app.borrow.services.wait_list.create_waitlist import add_to_wait_list
 from app.ratelimiter.limiter import limiter
+from dependency_injector.wiring import inject, Provide
+from app.dependency_injection.container import Container
 
 
 router = APIRouter(prefix="/borrows", tags=["borrows"])
 
 @router.post("/take")
 @limiter.limit("5/minute")
-async def Borrow_edition(request: Request, edition_id:int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@inject
+async def Borrow_edition(request: Request, edition_id:int,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user)):
     return await borrow_edition(uow=uow,token_data=token_data,edition_id=edition_id)
 
 @router.post("/return")
 @limiter.limit("5/minute")
-async def Return_borrow(request: Request, borrow_id:int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@inject
+async def Return_borrow(request: Request, borrow_id:int,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user)):
     return await return_borrow(uow=uow,token_data=token_data,borrow_id=borrow_id)
 
 @router.post("/waitlist", response_model=WaitlistResponse)
 @limiter.limit("5/minute")
-async def Add_to_waitlist(request: Request, edition_id:int,uow = Depends(get_uow),token_data = Depends(get_current_user)):
+@inject
+async def Add_to_waitlist(request: Request, edition_id:int,uow = Depends(Provide[Container.uow]),token_data = Depends(get_current_user)):
     return await add_to_wait_list(uow=uow,token_data=token_data,edition_id=edition_id)
