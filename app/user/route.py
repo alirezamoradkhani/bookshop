26f1daac17871cc5b4cc53d59a19b1c0848ae6fb
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-10 12:59:49 +0330

    fix(user/route): add depends

--- app/user/route.py
+++ app/user/route.py
@@ -18,19 +18,19 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.post("/singin")
 @limiter.limit("5/minute")
 @inject
-async def Verify_email(request: Request, email: str,uow = Provide[Container.uow]):
+async def Verify_email(request: Request, email: str,uow = Depends(Provide[Container.uow])):
     return await email_register(email=email,uow=uow)
     
 @router.post("/create", response_model= outputs.BaseUserResponse)
 @limiter.limit("5/minute")
 @inject
-async def Create_user(request: Request, user: inputs.UserCreate, otp:str, uow = Provide[Container.uow]):
+async def Create_user(request: Request, user: inputs.UserCreate, otp:str, uow = Depends(Provide[Container.uow])):
     return await create_user(uow=uow,user=user,otp=otp)
 
 @router.post("/login")
 @limiter.limit("5/minute")
 @inject
-async def login_step_one(request: Request, user: inputs.UserLogin,uow = Provide[Container.uow]):
+async def login_step_one(request: Request, user: inputs.UserLogin,uow = Depends(Provide[Container.uow])):
     return await login_by_user_pass(uow=uow,user=user)
 
 # @router.post("/login_by_otp", response_model=outputs.TokenResponse)
@@ -40,17 +40,17 @@ async def login_step_one(request: Request, user: inputs.UserLogin,uow = Provide[
 @router.delete("/delete", response_model= outputs.BaseUserResponse)
 @limiter.limit("5/minute")
 @inject
-async def Delete_account(request: Request, token_data: dict = Depends(get_current_user), uow = Provide[Container.uow]):
+async def Delete_account(request: Request, token_data: dict = Depends(get_current_user), uow = Depends(Provide[Container.uow])):
     return await delete_account(uow=uow,token_data=token_data)
 
 @router.patch("/plan",response_model= outputs.UserResponse)
 @limiter.limit("5/minute")
 @inject
-async def Upgrade_plan(request: Request, new_plan:inputs.UserPlanUpgrade,token_data: dict = Depends(get_current_user),uow = Provide[Container.uow]):
+async def Upgrade_plan(request: Request, new_plan:inputs.UserPlanUpgrade,token_data: dict = Depends(get_current_user),uow = Depends(Provide[Container.uow])):
     return await upgrade_plan(uow=uow,new_plan=new_plan, token_data=token_data)
 
 @router.get("/author/search",response_model=list[outputs.AuthorResponse])
 @limiter.limit("5/minute")
 @inject
-async def Search_author(request: Request, name:str | None=None,id:int | None=None,uow = Provide[Container.uow]):
+async def Search_author(request: Request, name:str | None=None,id:int | None=None,uow = Depends(Provide[Container.uow])):
     return await search_author(uow=uow,name=name,id=id)
