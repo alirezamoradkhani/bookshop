aafe8c9370feeb906804ac9c4f5d73bf9d73f5eb
Alireza moradkhani <moradkhani1386@gmail.com>
2026-05-11 10:49:22 +0330

    refactor(tasks): replace SessionLocal and UnitOfWork with dependency injection container

--- app/workers/tasks.py
+++ app/workers/tasks.py
@@ -6,25 +6,30 @@ from app.order.serivices.checks.mark_order_as_done import mark_ordere_as_done
 from app.user.services.checks.dowgrade_expired_plan import downgrade_expired_plan
 
 from app.borrow.services.checks.mark_borrow_as_overdue import mark_borrow_as_overdue
-from app.unit_of_work import UnitOfWork
-from app.database import SessionLocal
+from app.dependency_injection.container import Container
+
+container = Container()
 
 async def order_tasks():
-    async with SessionLocal() as db:
-        async with UnitOfWork(db) as uow:
-            await mark_orderedition_as_done(uow)
-            await mark_orderedition_as_rejected(uow)
-            await mark_orderedition_as_forcereject(uow)
-            await mark_ordere_as_done(uow)
-            await mark_ordere_as_inprogres(uow)
+    await container.init_resources()
+    uow_factory = container.uow
+    async with uow_factory() as uow:
+        await mark_orderedition_as_done(uow)
+        await mark_orderedition_as_rejected(uow)
+        await mark_orderedition_as_forcereject(uow)
+        await mark_ordere_as_done(uow)
+        await mark_ordere_as_inprogres(uow)
+    
 
 async def borrow_task():
-    async with SessionLocal() as db:
-        async with UnitOfWork(db) as uow:
-            await mark_borrow_as_overdue(uow)
+    await container.init_resources()
+    uow_factory = container.uow
+    async with uow_factory() as uow:
+        await mark_borrow_as_overdue(uow)
 
 
 async def plan_task():
-    async with SessionLocal() as db:
-        async with UnitOfWork(db) as uow:
-            await downgrade_expired_plan(uow)
+    await container.init_resources()
+    uow_factory = container.uow
+    async with uow_factory() as uow:
+        await downgrade_expired_plan(uow)
