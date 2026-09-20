352fd4c8140a540f41a78f207791d1eca37aff76
Alireza moradkhani <moradkhani1386@gmail.com>
2026-09-20 11:39:21 +0330

    refactor: colocate edition Mongo repositories

--- app/edition/repo/edition_language.py
+++ app/edition/repo/edition_language.py
@@ -1,1 +1,14 @@
-from app.mongo.repositories import EditionLanguageRepository
+from app.mongo.base import MongoRepository
+from app.edition.models.model import EditionLanguage
+from datetime import datetime, timedelta
+from app.mongo.serialization import serialize
+
+class EditionLanguageRepository(MongoRepository):
+    collection, model = "edition_languages", EditionLanguage
+
+    async def create(self, edition_language: EditionLanguage):
+        return await self._insert(edition_language)
+
+    async def create_many(self, items: list[EditionLanguage]):
+        for item in items:
+            await self._insert(item)
