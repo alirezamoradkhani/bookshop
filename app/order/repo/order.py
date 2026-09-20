f6537794790415daf49952d786c7844571df19e8
Alireza moradkhani <moradkhani1386@gmail.com>
2026-09-20 11:39:39 +0330

    refactor: colocate order Mongo repositories

--- app/order/repo/order.py
+++ app/order/repo/order.py
@@ -1,1 +1,32 @@
-from app.mongo.repositories import OrderRepository
+from app.mongo.base import MongoRepository
+from app.order.models.enums import OrderState
+from app.order.models.model import Order
+from datetime import datetime, timedelta
+from app.mongo.serialization import serialize
+
+class OrderRepository(MongoRepository):
+    collection, model = "orders", Order
+    enum_fields = {"state": OrderState}
+
+    async def create_order(self, order: Order):
+        return await self._insert(order)
+
+    async def get_by_id(self, order_id: int):
+        return await self._find_one({"_id": order_id})
+
+    async def update_order_state(self, order: Order, new_state: OrderState):
+        order.state = new_state
+
+    async def update_final_price(self, order: Order, change: int):
+        order.final_price -= change
+
+    async def many_update_state(self, order_ids: list[int], new_state):
+        await self.db.collection(self.collection).update_many(
+            {"_id": {"$in": order_ids}}, {"$set": {"state": serialize(new_state)}}, **self.db.options()
+        )
+
+    async def get_by_state(self, state: OrderState):
+        return await self._find({"state": serialize(state)})
+
+    async def get_by_user_id(self, id: int):
+        return await self._find({"user_id": id})
--- app/order/repo/order_edition.py
+++ app/order/repo/order_edition.py
@@ -1,1 +1,50 @@
-from app.mongo.repositories import OrderEditionRepository
+from app.mongo.base import MongoRepository
+from app.edition.models.model import Edition
+from app.order.models.enums import OrderItemState
+from app.order.models.model import OrderEdition
+from datetime import datetime, timedelta
+from app.mongo.serialization import serialize
+
+class OrderEditionRepository(MongoRepository):
+    collection, model = "order_editions", OrderEdition
+    enum_fields = {"state": OrderItemState}
+
+    async def create(self, orderedition: OrderEdition):
+        return await self._insert(orderedition)
+
+    async def create_many(self, order_editions: list[OrderEdition]):
+        for item in order_editions:
+            await self._insert(item)
+
+    async def update_state(self, new_state: OrderItemState, orderedition: OrderEdition):
+        orderedition.state = new_state
+        orderedition.last_modify = datetime.utcnow()
+
+    async def many_update_state(self, order_edition_ids: list[int], new_state: OrderItemState):
+        await self.db.collection(self.collection).update_many(
+            {"_id": {"$in": order_edition_ids}},
+            {"$set": {"state": serialize(new_state), "last_modify": datetime.utcnow()}},
+            **self.db.options(),
+        )
+
+    async def get_by_order_id(self, order_id: int):
+        return await self._find({"order_id": order_id})
+
+    async def get_by_order_edition_id(self, order_edition_id: int):
+        return await self._find_one({"_id": order_edition_id})
+
+    async def get_by_state(self, state: OrderItemState):
+        return await self._find({"state": serialize(state)})
+
+    async def get_by_last_modify(self, date: datetime):
+        start = date.replace(hour=0, minute=0, second=0, microsecond=0)
+        return await self._find({"last_modify": {"$gte": start, "$lt": start + timedelta(days=1)}})
+
+    async def get_by_last_modify_and_state(self, date: datetime, state: OrderItemState):
+        return await self._find({"last_modify": {"$lt": date}, "state": serialize(state)})
+
+    async def get_orderedition_by_list_of_edition(self, editions: list[Edition]):
+        return await self._find({"edition_id": {"$in": [edition.id for edition in editions]}})
+
+    async def get_by_order_ids(self, order_ids: list[int]):
+        return await self._find({"order_id": {"$in": order_ids}})
