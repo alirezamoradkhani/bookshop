d85bbe469fe08891d8e15af5dd9470960fe0632f
Alireza moradkhani <moradkhani1386@gmail.com>
2026-09-20 11:40:31 +0330

    refactor: wire domain repositories in unit of work

--- app/core/unit_of_work.py
+++ app/core/unit_of_work.py
@@ -1,21 +1,19 @@
 from app.mongo.database import MongoContext
-from app.mongo.repositories import (
-    AdminRepository,
-    AuthorRepository,
-    BaseUserRepository,
-    BookAuthorRepository,
-    BookCategoryRepository,
-    BookRepository,
-    Borrowpository,
-    EditionLanguageRepository,
-    EditionRepository,
-    OrderEditionRepository,
-    OrderRepository,
-    OutboxRepository,
-    TransactionRepository,
-    UserRepository,
-    Waitlistpository,
-)
+from app.user.repo.baseuser import BaseUserRepository
+from app.user.repo.user import UserRepository
+from app.user.repo.author import AuthorRepository
+from app.user.repo.admin import AdminRepository
+from app.book.repo.book import BookRepository
+from app.book.repo.book_author import BookAuthorRepository
+from app.book.repo.book_category import BookCategoryRepository
+from app.edition.repo.edition import EditionRepository
+from app.edition.repo.edition_language import EditionLanguageRepository
+from app.order.repo.order import OrderRepository
+from app.order.repo.order_edition import OrderEditionRepository
+from app.borrow.repo.borrow import Borrowpository
+from app.borrow.repo.waitlist import Waitlistpository
+from app.transaction.repo.transaction import TransactionRepository
+from app.outbox.repo import OutboxRepository
 
 
 class UnitOfWork:
